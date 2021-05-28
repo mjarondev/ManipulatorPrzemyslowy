53ec17dc8474d63bdc22ff34b7834e64d468b00d
mjaron <jaron19mi@gmail.com>
2021-05-28 20:05:16 +0200

    Added PositionAdd window.

---
+++ ManipulatorPrzemyslowy.Tests.Unit/PositionAddTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using System.Threading;
+
+namespace ManipulatorPrzemyslowy.Tests.Unit
+{
+    public class PositionAddTests
+    {
+        
+
+    }
+}
---
+++ ManipulatorPrzemyslowy.Tests.Unit/WindowTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using FluentAssertions;
+using System.Threading;
+using NUnit.Framework;
+using System.Windows;
+
+
+namespace ManipulatorPrzemyslowy.Tests.Unit
+{
+    class WindowTests
+    {
+        [Test]
+        public void test()
+        {
+            PositionAdd win;
+
+            var t = new Thread(() =>
+            {
+                win = new PositionAdd();
+
+                win.Closed += (s, e) => win.Dispatcher.InvokeShutdown();
+
+                win.Show();
+
+                System.Windows.Threading.Dispatcher.Run();
+
+            });
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+            t.Join();
+        }
+
+    }
+}
